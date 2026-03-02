fab3737441602f4572342d74220fa630047ee395
dbrighthd <dbrighthd@gmail.com>
2026-03-02 03:07:46 -0800

    Changed time to be a long instead of int, and check for paused game

--- src/client/java/dbrighthd/elytratrails/util/TimeUtil.java
+++ src/client/java/dbrighthd/elytratrails/util/TimeUtil.java
@@ -6,31 +6,23 @@ import net.minecraft.util.Util;
 
 public class TimeUtil {
 
-    static int tickCounter = 0;
+    static long tickCounter = 0;
     public static void init()
     {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            tickCounter++;
+            if (!client.isPaused())
+            {
+                tickCounter++;
+            }
+
         });
-    }
-    public static float currentTickPlusPartial()
-    {
-        return tickCounter + Minecraft.getInstance().getDeltaTracker().getGameTimeDeltaPartialTick(false);
     }
     public static long currentMillis()
     {
-        return (long)tickToMillis(currentTickPlusPartial());
+        return (long) (tickCounter*1000 + (Minecraft.getInstance().getDeltaTracker().getGameTimeDeltaPartialTick(false) * 1000))/20;
     }
     public static long currentNanos()
     {
-        return tickToNanos(currentTickPlusPartial());
-    }
-    public static float tickToMillis(float tickTime)
-    {
-        return (tickTime/20)*1000;
-    }
-    public static long tickToNanos(float tickTime)
-    {
-        return (long)(Util.NANOS_PER_MILLI * tickToMillis(tickTime));
+        return (Util.NANOS_PER_MILLI * currentMillis());
     }
 }
