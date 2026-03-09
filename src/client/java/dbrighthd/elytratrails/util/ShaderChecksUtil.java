5f8592d80f2830219dc7e8e722d3cc7fc77bbfa1
dbrighthd <dbrighthd@gmail.com>
2026-03-08 23:17:46 -0700

    Fix entityCutoutLit being broken without shaders

--- src/client/java/dbrighthd/elytratrails/util/ShaderChecksUtil.java
+++ src/client/java/dbrighthd/elytratrails/util/ShaderChecksUtil.java
@@ -6,13 +6,15 @@ import net.fabricmc.loader.api.FabricLoader;
 public final class ShaderChecksUtil {
     private ShaderChecksUtil() {}
 
+    private static final boolean IRIS_LOADED  = FabricLoader.getInstance().isModLoaded("iris");
+
     public static boolean isShadowPass() {
-        if (!FabricLoader.getInstance().isModLoaded("iris")) return false;
+        if (!IRIS_LOADED) return false;
         return IrisCompat.isShadowPassing();
     }
     @SuppressWarnings("unused")
     public static boolean isUsingShaders() {
-        if (!FabricLoader.getInstance().isModLoaded("iris")) return false;
+        if (!IRIS_LOADED) return false;
         return IrisCompat.isUsingShaders();
     }
 }
