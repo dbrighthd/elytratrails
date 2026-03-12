899624d7bb3e55769bf724c3f6ebc28325976da5
dbrighthd <dbrighthd@gmail.com>
2026-03-12 01:00:51 -0700

    Remove flashback replay pause check, wasn't working as intended

--- src/client/java/dbrighthd/elytratrails/util/TimeUtil.java
+++ src/client/java/dbrighthd/elytratrails/util/TimeUtil.java
@@ -1,9 +1,6 @@
 package dbrighthd.elytratrails.util;
 
-
-import dbrighthd.elytratrails.compat.flashback.FlashbackCompat;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.Util;
 
@@ -11,13 +8,9 @@ public class TimeUtil {
 
     private static long tickCounter = 0;
 
-    public static final boolean FLASHBACK_LOADED =
-            FabricLoader.getInstance().isModLoaded("flashback");
-
-
     public static void init() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (!shouldAdvanceTickCounter(client)) {
+            if (client.isPaused()) {
                 return;
             }
 
@@ -25,17 +18,8 @@ public class TimeUtil {
         });
     }
 
-    private static boolean shouldAdvanceTickCounter(Minecraft client) {
-        if (client.isPaused()) {
-            return false;
-        }
-
-        return !FLASHBACK_LOADED || !FlashbackCompat.isReplayPaused();
-    }
-
     public static long currentMillis() {
-        return (long) (tickCounter * 1000
-                + (Minecraft.getInstance().getDeltaTracker().getGameTimeDeltaPartialTick(false) * 1000)) / 20;
+        return (long) (tickCounter * 1000 + (Minecraft.getInstance().getDeltaTracker().getGameTimeDeltaPartialTick(false) * 1000)) / 20;
     }
 
     public static long currentNanos() {
