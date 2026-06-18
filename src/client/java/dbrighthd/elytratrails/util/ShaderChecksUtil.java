42dd7d297fed556aec6630f726ae84f299b58d0f
dbrighthd <dbrighthd@gmail.com>
2026-06-18 01:52:49 -0700

    Fix shader checks util

--- src/client/java/dbrighthd/elytratrails/util/ShaderChecksUtil.java
+++ src/client/java/dbrighthd/elytratrails/util/ShaderChecksUtil.java
@@ -1,6 +1,7 @@
 package dbrighthd.elytratrails.util;
 
 //import dbrighthd.elytratrails.compat.IrisCompat;
+import dbrighthd.elytratrails.compat.IrisCompat;
 import net.fabricmc.loader.api.FabricLoader;
 
 public final class ShaderChecksUtil {
@@ -10,15 +11,13 @@ public final class ShaderChecksUtil {
     private static final boolean IRIS_LOADED = FabricLoader.getInstance().isModLoaded("iris");
 
     public static boolean isShadowPass() {
-//        if (!IRIS_LOADED) return false;
-//        return IrisCompat.isShadowPassing();
-        return false;
+        if (!IRIS_LOADED) return false;
+        return IrisCompat.isShadowPassing();
     }
 
     @SuppressWarnings("unused")
     public static boolean isUsingShaders() {
-//        if (!IRIS_LOADED) return false;
-//        return IrisCompat.isUsingShaders();
-        return false;
+        if (!IRIS_LOADED) return false;
+        return IrisCompat.isUsingShaders();
     }
 }
